cb976d418c31eae4af3a36c620c22d8980fc6a42
Aratz <ahernandezj23@fpsanjorge.com>
2024-01-11 14:25:33 +0100

    cambios api

--- talde1erronka2/app/Http/Controllers/ControladorOrdutegia.php
+++ talde1erronka2/app/Http/Controllers/ControladorOrdutegia.php
@@ -24,7 +24,7 @@ class ControladorOrdutegia extends Controller
 
     public function insert(Request $request){
             $datos=$request->all();
-            $data=["kodea"=>$datos["kodea"],"eguna"=>$datos["eguna"],"hasiera_data"=>$datos["hasiera_data"],"amaiera_data"=>$datos["amaiera_data"],"hasiera_ordua"=>$datos["hasiera_ordua"],"amaiera_ordua"=>$datos["amaiera_ordua"]];
+            $data=["kodea"=>$datos["kodea"],"eguna"=>$datos["eguna"],"hasiera_data"=>$datos["hasiera_data"],"amaiera_data"=>$datos["amaiera_data"],"hasiera_ordua"=>$datos["hasiera_ordua"],"amaiera_ordua"=>$datos["amaiera_ordua"],"sortze_data" =>$datos["sortze_data"]];
             Ordutegia::insert($data);
            // return "Se ha insertado";
     }
@@ -32,11 +32,11 @@ class ControladorOrdutegia extends Controller
     public function update(Request $request){
         
         $datos=$request->all();
-        $belajar = Ordutegia::find($datos[0]);
+        $belajar = Ordutegia::find($datos["id"]);
         if(!$belajar){
             return response()->json(['errors' => "No existe",], 404);
         }else{
-            Ordutegia::where('id', $datos['id'])->update(array('kodea' =>$datos['kodea'],'eguna' =>$datos['eguna'],'hasiera_data' =>$datos['hasiera_data'],'amaiera_data' =>$datos['amaiera_data'],'hasiera_ordua' =>$datos['hasiera_ordua' ],'amaiera_ordua' =>$datos['amaiera_ordua'],'eguneratze_data' =>$datos['ezabatze_data']));
+            Ordutegia::where('id', $datos['id'])->update(array('kodea' =>$datos['kodea'],'eguna' =>$datos['eguna'],'hasiera_data' =>$datos['hasiera_data'],'amaiera_data' =>$datos['amaiera_data'],'hasiera_ordua' =>$datos['hasiera_ordua' ],'amaiera_ordua' =>$datos['amaiera_ordua'],'eguneratze_data' =>$datos['eguneratze_data']));
         }
     }
 

bb23e8d5dfdbbeb1d74991139015cb1ede3ddf65
Aratz <ahernandezj23@fpsanjorge.com>
2024-01-11 13:15:53 +0100

    routes

--- talde1erronka2/routes/api.php
+++ talde1erronka2/routes/api.php
@@ -22,23 +22,23 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/langileaklortu","App\Http\Controllers\ControladorLangile@erakutzi");
-Route::post("/langileaksortu","App\Http\Controllers\ControladorLangile@insert");
-Route::put("/langileakeguneratu","App\Http\Controllers\ControladorLangile@update");
-Route::get('/langileaklortubyid/{id}', [ControladorLangile::class, 'erakutzibyid']);
+Route::get("/langileak","App\Http\Controllers\ControladorLangile@erakutzi");
+Route::post("/langileak","App\Http\Controllers\ControladorLangile@insert");
+Route::put("/langileak","App\Http\Controllers\ControladorLangile@update");
+Route::get('/langileakbyid/{id}', [ControladorLangile::class, 'erakutzibyid']);
 Route::delete("/langileakdelete","App\Http\Controllers\ControladorLangile@delete");
 
-Route::get("/ordutegiaklortu","App\Http\Controllers\ControladorOrdutegia@erakutzi");
-Route::post("/ordutegiaksortu","App\Http\Controllers\ControladorOrdutegia@insert");
-Route::put("/ordutegiakeguneratu","App\Http\Controllers\ControladorOrdutegia@update");
+Route::get("/ordutegiak","App\Http\Controllers\ControladorOrdutegia@erakutzi");
+Route::post("/ordutegiak","App\Http\Controllers\ControladorOrdutegia@insert");
+Route::put("/ordutegiak","App\Http\Controllers\ControladorOrdutegia@update");
 Route::get('/ordutegiakortubyid/{id}', [ControladorOrdutegia::class, 'erakutzibyid']);
-Route::delete("/ordutegiakdelete","App\Http\Controllers\ControladorOrdutegia@delete");
+Route::delete("/ordutegiak","App\Http\Controllers\ControladorOrdutegia@delete");
 
-Route::get("/taldeaklortu","App\Http\Controllers\\talde_controller@taldeak_kargatu");
+Route::get("/taldeak","App\Http\Controllers\\talde_controller@taldeak_kargatu");
 Route::get("/taldeaklortubycode/{kodea}","App\Http\Controllers\\talde_controller@taldeak_kargatu_byid");
 Route::post("/taldeaksortu","App\Http\Controllers\\talde_controller@taldea_insert");
-Route::put("/taldeakeguneratu","App\Http\Controllers\\talde_controller@taldea_update");
-Route::delete("/taldeakdelete","App\Http\Controllers\\talde_controller@taldea_delete");
+Route::put("/taldeak","App\Http\Controllers\\talde_controller@taldea_update");
+Route::delete("/taldeak","App\Http\Controllers\\talde_controller@taldea_delete");
 
 
 
